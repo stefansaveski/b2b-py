68fe6ae87e25e3cdd238ae5740445d3b8d76c922
Stefan Saveski <stefansaveski19@gmail.com>
2023-12-21 21:42:20 +0100

    v1.4

--- server.py
+++ server.py
@@ -12,14 +12,10 @@ def index():
 
 @app.route('/zadaca1/')
 def zadaca1():
-    if request.method == 'POST':
-        flash('Action successful', 'success')
-        return redirect(url_for('zadaca1'))
     return render_template('zadaca1.html')
 
 @app.route('/zadaca2/')
 def zadaca2():
-    print(session["index"])
     return render_template('zadaca2.html')
 
 @app.route('/zadaca3/')
@@ -36,10 +32,9 @@ def process_code():
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     print(data)
-    if 'index' in session:
+    try:
         return start(data, session["index"], session["name"], session['start_time'], current_time)
-    else:
-        # Handle the case where 'index' is not present in the session
+    except:
         return {"input": ' ', "expected": ' ',"got": ' ', "is_same": ' ', "correct": ' ', "CompilationError": 'Server error try again'}
 
 @app.route('/login', methods=['POST'])
@@ -52,6 +47,27 @@ def login():
     current_time = now.strftime("%H:%M:%S")
     session['start_time'] = current_time
     return "true"
+
+@app.route('/get_page', methods=['POST'])
+def get_page():
+    data =  request.get_json()
+    code = "code" + str(data.get("jsonData", {}).get("zadaca", ''))
+    table = "table" + str(data.get("jsonData", {}).get("zadaca", ''))
+    if code in session and table in session:
+        return {"code":session[code], "table":session[table], "get":" "}
+    else:
+        return {"code":" ", "table":" ", "get":"empty"}
+ 
+@app.route('/save_page', methods=['POST'])
+def save_page():
+    data = request.get_json()
+    zadaca = data.get("jsonData", {}).get("zadaca", '')
+    print(data.get("jsonData", {}).get("table", ''))
+    session["code"+str(zadaca)] = data.get("jsonData", {}).get("cpp_code", '')
+    session["table" + str(zadaca)] = data.get("jsonData", {}).get("table", '')
+    print(session["table"+str(zadaca)])
+    return "true"
+
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
--- static/getresults.js
+++ static/getresults.js
@@ -1,87 +1,31 @@
-async function saveInfo(){
-  try {
-      var firstName = document.getElementById("firstName").value; 
-      var lastName = document.getElementById("lastName").value; 
-      var index = document.getElementById("indexNumber").value; 
-      var jsonData = {
-        "index": index,
-        "name": firstName + " " + lastName,
-      }
-      const response = await fetch('/login', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ jsonData }),
-      });
-  } catch (error) {
-      console.error('Error:', error);
-  }
+window.onload = loadPage;
+async function loadPage(){
+  var zadaca = window.location.pathname.replace(/\/$/, '').split('/').pop();
+var location = document.getElementById("results");
+jsonData = {
+  "zadaca": zadaca
 }
+const response = await fetch('/get_page', {
+  method: 'POST',
+  headers: {
+      'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ jsonData }),
+});
+const data = await response.json()
+console.log(data);
+if(data.get != "empty"){
+  //location.appendChild(data.table);
+  document.getElementById('code').value = data.code;
 
-function checkfields(){
-  var firstName = document.getElementById("firstName").value; 
-  var lastName = document.getElementById("lastName").value; 
-  var index = document.getElementById("indexNumber").value;
-  if(firstName == "" || lastName == "" || index == ""){
-      alert("Fill all the fields!");
-  }
-  else{
-      location.href = `${window.location.protocol}//${window.location.host}/zadaca1`; 
-      saveInfo();
-  } 
-}
-
-async function submitCode() {
-  var currentPageName = window.location.pathname.replace(/\/$/, '').split('/').pop();
-  const code = document.getElementById('code').value;
-  var jsonData = {
-      "cpp_code": code.replace(/\n/g, "\n"),
-      "zadaca": currentPageName
-  }
-  try {
-      const response = await fetch('/process_code', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ jsonData }),
-      });
-
-      const data = await response.json()
-      return data;
-     // document.getElementById('result').innerText = 'Result: ${data.result}';
-  } catch (error) {
-      console.error('Error:', error);
-  }
-}
-
-async function displayDataInTable(buttonId) {
+  
   var location = document.getElementById("results");
-  location.innerHTML = '<div class="loader"></div>';
-  const button = document.getElementById(buttonId);
-  //button.disabled = true;
-  jsonData = await submitCode();
+  jsonData = data.table;
+  console.log(jsonData)
   const CE = jsonData.CompilationError;
     try {
       var table = document.createElement("table");
       table.style.borderCollapse = "collapse"; 
-      location.innerHTML = "";
-      if(CE == 'Compilation Error'){
-        button.disabled = false;
-        location.innerHTML = '<div class="h4" style="color: #780116">Compilation Error</div>';
-        return;
-      }
-      else if(CE == 'Time Limit Exceeded'){
-        button.disabled = false;
-        location.innerHTML = '<div class="h4" style="color: #780116">Time Limit Exceeded</div>';
-        return;
-      }
-      else if(CE == 'Server error try again'){
-        button.disabled = false;
-        location.innerHTML = '<div class="h4" style="color: #780116">Server error try again</div>';
-        return;
-      }
       var headerRow = table.insertRow();
       ["Input", "Expected", "Got"].forEach((header) => {
         var th = document.createElement("th");
@@ -148,5 +92,183 @@ async function displayDataInTable(buttonId) {
     } catch (error) {
       console.error("Error in displayDataInTable:", error);
     }
-  
+
+
+}
+}
+
+
+async function saveInfo(){
+  try {
+      var firstName = document.getElementById("firstName").value; 
+      var lastName = document.getElementById("lastName").value; 
+      var index = document.getElementById("indexNumber").value; 
+      var jsonData = {
+        "index": index,
+        "name": firstName + " " + lastName,
+      }
+      const response = await fetch('/login', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ jsonData }),
+      });
+  } catch (error) {
+      console.error('Error:', error);
+  }
+}
+
+function checkfields(){
+  var firstName = document.getElementById("firstName").value; 
+  var lastName = document.getElementById("lastName").value; 
+  var index = document.getElementById("indexNumber").value;
+  if(firstName == "" || lastName == "" || index == ""){
+      alert("Fill all the fields!");
+  }
+  else{
+      location.href = `${window.location.protocol}//${window.location.host}/zadaca1`; 
+      saveInfo();
+  } 
+}
+
+async function submitCode() {
+  var currentPageName = window.location.pathname.replace(/\/$/, '').split('/').pop();
+  const code = document.getElementById('code').value;
+  var jsonData = {
+      "cpp_code": code.replace(/\n/g, "\n"),
+      "zadaca": currentPageName
+  }
+  try {
+      const response = await fetch('/process_code', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ jsonData }),
+      });
+
+      const data = await response.json()
+      return data;
+     // document.getElementById('result').innerText = 'Result: ${data.result}';
+  } catch (error) {
+      console.error('Error:', error);
+  }
+}
+
+async function displayDataInTable(buttonId) {
+  var savetable;
+  var savecode;
+  var location = document.getElementById("results");
+  location.innerHTML = '<div class="loader"></div>';
+  const button = document.getElementById(buttonId);
+  button.disabled = true;
+  jsonData = await submitCode();
+  const CE = jsonData.CompilationError;
+    try {
+      var table = document.createElement("table");
+      table.style.borderCollapse = "collapse"; 
+      location.innerHTML = "";
+      if(CE == 'Compilation Error'){
+        button.disabled = false;
+        location.innerHTML = '<div class="h4" style="color: #780116">Compilation Error</div>';
+        return;
+      }
+      else if(CE == 'Time Limit Exceeded'){
+        button.disabled = false;
+        location.innerHTML = '<div class="h4" style="color: #780116">Time Limit Exceeded</div>';
+        return;
+      }
+      else if(CE == 'Server error try again'){
+        button.disabled = false;
+        location.innerHTML = '<div class="h4" style="color: #780116">Server error reload page and try again</div>';
+        return;
+      }
+      var headerRow = table.insertRow();
+      ["Input", "Expected", "Got"].forEach((header) => {
+        var th = document.createElement("th");
+        th.appendChild(document.createTextNode(header));
+        th.style.border = "1px solid black"; 
+        th.style.padding = "15px";
+        headerRow.appendChild(th);
+      });
+
+      let flag = true;
+      const keys = Object.keys(jsonData.input);
+      const halfLength = Math.ceil(keys.length / 2);
+      for (let i = 0; i < halfLength; i++) {
+        const key = keys[i];
+        var row = table.insertRow();
+        row.style.border = "1px solid black"; 
+
+        var inputCell = row.insertCell();
+        inputCell.style.border = "1px solid black"; 
+        inputCell.style.padding = "10px";
+        var inputText = jsonData.input[key].replace(/\n/g, "<br>");
+        inputCell.innerHTML = inputText;
+
+        var expectedCell = row.insertCell();
+        expectedCell.style.border = "1px solid black";
+        expectedCell.style.padding = "10px";
+        var expectedText = jsonData.expected[key].replace(/\n/g, "<br>");
+        expectedCell.innerHTML = expectedText;
+
+        var gotCell = row.insertCell();
+        gotCell.style.border = "1px solid black"; 
+        gotCell.style.padding = "10px";
+        var gotText = jsonData.got[key].replace(/\n/g, "<br>");
+        gotCell.innerHTML = gotText;
+
+        if (jsonData.is_same[key] === "true") {
+          row.style.backgroundColor = "lightgreen";
+        } else {
+          row.style.backgroundColor = "#FFCCCB";
+          flag = false;
+        }
+      }
+      if (jsonData.correct == "10") {
+        var row = table.insertRow();
+        var resultCell = row.insertCell();
+        resultCell.style.padding = "12px";
+        resultCell.colSpan = 3;
+        resultCell.style.border = "1px solid black"; 
+        resultCell.style.color = "green";
+        resultCell.style.fontWeight = "800";
+        resultCell.appendChild(document.createTextNode("Passed all tests!"));
+      } else {
+        button.disabled = false;
+        var row = table.insertRow();
+        var resultCell = row.insertCell();
+        resultCell.style.padding = "12px";
+        resultCell.colSpan = 3; 
+        resultCell.style.border = "1px solid black"; 
+        resultCell.style.color = "red";
+        resultCell.style.fontWeight = "800";
+        resultCell.appendChild(document.createTextNode("Didn't pass all tests"));
+      }
+      savetable = table;
+      savecode = document.getElementById('code').value;
+      //console.log(savetable, savecode);
+      savePage(jsonData, savecode);
+      location.appendChild(table);
+    } catch (error) {
+      console.error("Error in displayDataInTable:", error);
+    }
+}
+
+async function savePage(jsonData, code){
+  var zadaca = window.location.pathname.replace(/\/$/, '').split('/').pop();
+  jsonData = {
+    "table": jsonData,
+    "cpp_code": code,
+    "zadaca": zadaca
+  }
+  console.log(jsonData);
+  const response = await fetch('/save_page', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ jsonData }),
+  });
 }
